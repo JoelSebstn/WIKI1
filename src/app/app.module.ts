38d9fdd3f4847b30db7162470d7d45a2f2864672
Joel Sebastian <joel.sebastian@experionglobal.local>
2021-02-08 19:58:19 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from '@angular/common/http';
+import { FormsModule,NgForm} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputComponent } from './input/input.component';
+import { ResultComponent } from './result/result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/input/input.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css'],
+})
+export class InputComponent implements OnInit {
+  
+  @Output() searchedName = new EventEmitter<string>();
+
+  name: string = '';
+
+  search(event: any) {
+    event.preventDefault();
+    this.searchedName.emit(this.name);
+  }
+  ngOnInit(): void {}
+}
